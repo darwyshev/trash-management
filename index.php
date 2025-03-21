cceb5ad6603e1b4fb9a2d73a2626d33bee93aa7d
darius <prayogadariusw@gmail.com>
2025-03-22 05:01:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manajemen Sampah Plastik</title>
+    <title>Plastic Garbage Management</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lux/bootstrap.min.css">
     <style>
         /* Styling untuk Hero Image */
